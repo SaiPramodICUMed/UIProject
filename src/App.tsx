2f7e90d8963e8d4811d1b8fb4ed148a3ead9f573
naveenmedidha16 <naveen.medidha@navasoftware.com>
2025-10-13 14:23:20 +0530

    reverted routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ import LayOut from "./layout/layout";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Inbox from "./pages/Inbox/Inbox";
 import Inprogress from "./pages/Inbox/Inprogress";
-import Drafts from "./pages/Inbox/Drafts";
-import AwaitingResults from "./pages/Inbox/AwaitingResults";
-import Completed from "./pages/Inbox/Completed";
-import Cancelled from "./pages/Inbox/Cancelled";
-import Trash from "./pages/Inbox/Trash";
-import All from "./pages/Inbox/All";
 function App() {
   
 
@@ -23,12 +17,6 @@ function App() {
       <Route path="/" element={< Inbox/>} />
       <Route path="/inbox" element={< Inbox/>} />
       <Route path="/inprogress" element={< Inprogress/>} />
-      <Route path="/drafts" element={< Drafts/>} />
-      <Route path="/awaitingResults" element={<AwaitingResults/>} />
-      <Route path="/completed" element={< Completed/>} />
-      <Route path="/cancelled" element={< Cancelled/>} />
-      <Route path="/trash" element={< Trash/>} />
-      <Route path="/all" element={< All/>} />
 
       {/* <Route path="/GateDashboard" element={<GateDashboard />} />
 
