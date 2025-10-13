e77c9deb0f89fd5d54aeb6e0940f18c79a1c7097
naveenmedidha16 <naveen.medidha@navasoftware.com>
2025-10-13 14:18:09 +0530

    added more routes for inbox

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import LayOut from "./layout/layout";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Inbox from "./pages/Inbox/Inbox";
 import Inprogress from "./pages/Inbox/Inprogress";
+import Drafts from "./pages/Inbox/Drafts";
+import AwaitingResults from "./pages/Inbox/AwaitingResults";
+import Completed from "./pages/Inbox/Completed";
+import Cancelled from "./pages/Inbox/Cancelled";
+import Trash from "./pages/Inbox/Trash";
+import All from "./pages/Inbox/All";
 function App() {
   
 
@@ -17,6 +23,12 @@ function App() {
       <Route path="/" element={< Inbox/>} />
       <Route path="/inbox" element={< Inbox/>} />
       <Route path="/inprogress" element={< Inprogress/>} />
+      <Route path="/drafts" element={< Drafts/>} />
+      <Route path="/awaitingResults" element={<AwaitingResults/>} />
+      <Route path="/completed" element={< Completed/>} />
+      <Route path="/cancelled" element={< Cancelled/>} />
+      <Route path="/trash" element={< Trash/>} />
+      <Route path="/all" element={< All/>} />
 
       {/* <Route path="/GateDashboard" element={<GateDashboard />} />
 
