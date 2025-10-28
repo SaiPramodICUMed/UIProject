d9ad586452a119fb3a1268996fe9601dcbf4ed51
Sai Pramod <sai.pramod@icumed.com>
2025-10-28 15:20:16 +0530

    changes

--- src/pages/Inbox/Inprogress.tsx
+++ src/pages/Inbox/Inprogress.tsx
@@ -55,7 +55,7 @@ const Inprogress: React.FC = () => {
 
   const fetchData = async (arg: any, start: number, end: number) => {
     console.log(arg, start, end);
-   // setLoading(true);
+    setLoading(true);
     //setActiveTab(arg);
     try {
       const payload = {
